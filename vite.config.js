f17e041e4b513eb9f8dec6bcdf8ba64a57afe877
mohammed karim dahmani <mohammedkarimdahmani@gmail.com>
2025-06-18 18:03:47 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  // <-- ajoute cette ligne
+  base: '/herafi/',
+
   plugins: [vue()],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') }
